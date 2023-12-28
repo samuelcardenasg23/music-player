4c5b6603e14fe47742b8e2d6464728b576e72913
Samuel Cardenas <samuelcardenas1706@gmail.com>
2023-12-28 11:59:13 -0500

    Play the first song when the play button is clicked

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -87,6 +87,21 @@ let userData = {
     songCurrentTime: 0
 };
 
+const playSong = (id) => {
+    const song = userData?.songs.find(song => song.id === id);
+    audio.src = song.src;
+    audio.title = song.title;
+    if (userData?.currentSong === null || userData?.currentSong.id !== song.id){
+        audio.currentTime = 0;
+    } else {
+        audio.currentTime = userData.songCurrentTime;
+    }
+
+    userData.currentSong = song;
+    playButton.classList.add('playing');
+    audio.play();
+}
+
 const renderSongs = (array) => {
     const songsHTML = array.map((song) => {
         return `
@@ -104,5 +119,13 @@ const renderSongs = (array) => {
     }).join("");
     playlistSongs.innerHTML = songsHTML;
 }
+
+playButton.addEventListener('click', () => {
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
+    }
+});
 
 renderSongs(userData?.songs);
