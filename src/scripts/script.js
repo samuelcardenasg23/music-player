b52afe183ca2d59e78fe691d65f9a0c4512b2bfd
Samuel Cardenas <samuelcardenas1706@gmail.com>
2023-12-28 17:42:21 -0500

    Previous, next and pause button works. Highlight the current song.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -99,14 +99,42 @@ const playSong = (id) => {
 
     userData.currentSong = song;
     playButton.classList.add('playing');
+    highlightCurrentSong();
     audio.play();
 }
+
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+
+    playButton.classList.remove('playing');
+    audio.pause();
+};
+
+const playNextSong = () => {
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        const currentSongIndex = getCurrentSongIndex();
+        const nextSong = userData?.songs[currentSongIndex + 1];
+        playSong(nextSong.id);
+    }
+};
+
+const playPreviousSong = () => {
+    if (userData?.currentSong === null) {
+        return;
+    } else {
+        const currentSongIndex = getCurrentSongIndex();
+        const previousSong = userData?.songs[currentSongIndex - 1];
+        playSong(previousSong.id);
+    }
+};
 
 const renderSongs = (array) => {
     const songsHTML = array.map((song) => {
         return `
         <li id="song-${song.id}" class="playlist-song">
-            <button class="playlist-song-info">
+            <button class="playlist-song-info" onclick="playSong(${song.id})">
                 <span class="playlist-song-title">${song.title}</span>
                 <span class="playlist-song-artist">${song.artist}</span>
                 <span class="playlist-song-duration">${song.duration}</span>
@@ -120,6 +148,23 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songsHTML;
 }
 
+const highlightCurrentSong = () => {
+    const playlistSongElements = document.querySelectorAll('.playlist-song');
+    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute("aria-current");
+    });
+
+    if (songToHighlight) {
+        songToHighlight.setAttribute("aria-current", "true");
+    }
+};
+
+
+
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData.currentSong);
+
 playButton.addEventListener('click', () => {
     if (userData?.currentSong === null) {
         playSong(userData?.songs[0].id);
@@ -127,5 +172,11 @@ playButton.addEventListener('click', () => {
         playSong(userData?.currentSong.id);
     }
 });
+
+pauseButton.addEventListener('click', pauseSong);
+
+nextButton.addEventListener('click', playNextSong);
+
+previousButton.addEventListener('click', playPreviousSong);
 
 renderSongs(userData?.songs);
